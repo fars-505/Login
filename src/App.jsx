daf5fece66eabf1374b89221131004247001b8c3
fars-505 <fzpurnama20@gmail.com>
2026-04-28 14:13:38 +0700

    update project login frontend

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,6 @@
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import Page from "./pages/LoginPage";
-import Dashboard from "./pages/Dashboard";
+import Dashboard from "../pages/Dashboard";
 
 function App() {
   return (
